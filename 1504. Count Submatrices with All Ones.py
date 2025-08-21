442a9326ba214e41c757a3cde62624d0b8a86058
shivam100 <thakurshivam100@gmail.com>
2025-08-20 22:40:58 -0700

    08/20 Daily Challenge

---
+++ 1504. Count Submatrices with All Ones.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/count-submatrices-with-all-ones/description/?envType=daily-question&envId=2025-08-21
+from typing import List
+class Solution:
+  def numSubmat(self, mat: list[list[int]]) -> int:
+    ans = 0
+    hist = [0] * len(mat[0])
+
+    for row in mat:
+      for i, num in enumerate(row):
+        hist[i] = 0 if num == 0 else hist[i] + 1
+      ans += self._count(hist)
+
+    return ans
+
+  def _count(self, hist: list[int]) -> int:
+    # submatrices[i] := the number of submatrices, where the i-th column is the
+    # right border
+    submatrices = [0] * len(hist)
+    stack = []
+
+    for i, h in enumerate(hist):
+      while stack and hist[stack[-1]] >= h:
+        stack.pop()
+      if stack:
+        prevIndex = stack[-1]
+        submatrices[i] = submatrices[prevIndex] + h * (i - prevIndex)
+      else:
+        submatrices[i] = h * (i + 1)
+      stack.append(i)
+
+    return sum(submatrices)
+
+sol = Solution()
+assert sol.numSubmat([[1,0,1],[1,1,0],[1,1,0]]) == 13
+assert sol.numSubmat([[0,1,1,0],[0,1,1,1],[1,1,1,0]]) == 24 
