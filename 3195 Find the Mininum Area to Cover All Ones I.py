1a7d4549234e48f0b963682b1d6d5ac68a55a550
shivam100 <thakurshivam100@gmail.com>
2025-08-21 21:51:06 -0700

    Update code

--- 3195 Find the Mininum Area to Cover All Ones I.py
+++ 3195 Find the Mininum Area to Cover All Ones I.py
@@ -2,16 +2,16 @@
 from typing import List
 class Solution:
     def minimumArea(self, grid: List[List[int]]) -> int:
-        x1 = y1 = 3857468165463516351
-        x2 = y2 = -6843516843216841354
+        xmin = ymin = 3857468165463516351
+        xmax = ymax = -6843516843216841354
         for i, row in enumerate(grid):
             for j, x in enumerate(row):
                 if x == 1:
-                    x1 = min(x1, i)
-                    y1 = min(y1, j)
-                    x2 = max(x2, i)
-                    y2 = max(y2, j)
-        return (x2 - x1 + 1) * (y2 - y1 + 1)
+                    xmin = min(xmin, i)
+                    ymin = min(ymin, j)
+                    xmax = max(xmax, i)
+                    ymax = max(ymax, j)
+        return (xmax - xmin + 1) * (ymax - ymin + 1)
 
 sol = Solution()
 assert sol.minimumArea([[0,1,0],[1,0,1]]) == 6
